4a6bea90cb2265fc0cec52470f5ca2af57dff332
Harald Lang <harald.lang@gmail.com>
2016-09-04 01:33:30 +0200

    add initial 'tree mask' implementation using balanced parentheses

--- bitmask.hpp
+++ bitmask.hpp
@@ -1,79 +1,277 @@
 #pragma once
 
-#include <bitset>
+#include "adept.hpp"
 #include "math.hpp"
+#include <array>
+#include <bitset>
+#include <functional>
+#include <vector>
 
-#include <iostream> //TODO remove
+namespace tree {
 
-template<size_t n>
-void print(const std::bitset<n>& b) {
-  for (size_t i = 0; i < n; i++) {
-    std::cout << b[i];
-  }
-  std::cout << std::endl;
-}
+  template<u64 N>
+  class tree_mask {
+  private:
+    static_assert(is_power_of_two(N), "Template parameter 'N' must be a power of two.");
 
-template<size_t n>
-void print(const std::bitset<n>& b, const size_t l) {
-  const size_t x = 1 << l;
-  for (size_t i = 0; i < n; i++) {
-    if (i % x == 0) {
-      std::cout << b[i];
+    static constexpr u64 length = 2 * N - 1;
+    static constexpr u64 height = ct::log_2<N>::value;
+    std::bitset<length> is_inner_node;
+    std::bitset<length> bit;
+    std::array<$u32, length> false_positive_cnt {};
+
+
+    static inline u64 parent_of(u64 idx) {
+      return (idx - 1) / 2;
     }
-    else {
-      std::cout << "_";
+    static inline u64 left_child_of(u64 idx) {
+      return 2 * idx + 1;
     }
-  }
-  std::cout << std::endl;
-}
-
-template<typename T>
-void print(const std::vector<T>& v, const size_t l) {
-  const size_t x = 1 << l;
-  for (size_t i = 0; i < v.size(); i++) {
-    if (i % x == 0) {
-//      std::cout << v[v.size() - (i + 1)] << ", ";
-      std::cout << v[i] << ", ";
+    static inline u64 right_child_of(u64 idx) {
+      return 2 * idx + 2;
     }
-    else {
-      std::cout << "_, ";
+    static inline u64 level_of(u64 idx) {
+      return log_2(idx + 1);
     }
-  }
-  std::cout << std::endl;
-}
 
-template<size_t num_input_bits, size_t num_output_bits>
-std::bitset<num_output_bits> compress(const std::bitset<num_input_bits>& bitmask) {
-  static_assert(is_power_of_two(num_input_bits), "Template parameter 'num_input_bits'  must be a power of two.");
-  static_assert(is_power_of_two(num_output_bits), "Template parameter 'num_output_bits'  must be a power of two.");
+    explicit tree_mask(const std::bitset<N> bitmask) {
+      false_positive_cnt.fill(0);
+      // initialize a complete binary tree
+      // ... all the inner nodes have two children
+      for ($u64 i = 0; i < length / 2; i++) {
+        is_inner_node[i] = true;
+      }
+      // ... the leaf nodes are labelled with the given bitmask
+      for ($u64 i = length / 2; i < length; i++) {
+        is_inner_node[i] = false;
+        bit[i] = bitmask[i - length / 2];
+      }
+      // propagate the mask bits along the tree (bottom-up)
+      for ($u64 i = 0; i < length - 1; i++) {
+        u64 node_idx = length - i - 1;
+        bit[parent_of(node_idx)] = bit[parent_of(node_idx)] | bit[node_idx];
+      }
+      // bottom-up pruning (loss-less)
+      for ($u64 i = 0; i < length - 1; i += 2) {
+        u64 left_node_idx = length - i - 2;
+        u64 right_node_idx = left_node_idx + 1;
 
-  const size_t n = ct::log_2<num_input_bits>::value;
+        u1 left_bit = bit[left_node_idx];
+        u1 right_bit = bit[right_node_idx];
 
-  size_t num_nodes = 2 * num_input_bits - 1;
+        u64 parent_node_idx = parent_of(left_node_idx);
+        false_positive_cnt[parent_node_idx] = false_positive_cnt[left_node_idx] + false_positive_cnt[right_node_idx];
 
-  std::cout << std::endl;
-
-  std::bitset<num_input_bits> b(bitmask);
-  std::bitset<num_output_bits> compressed_bitmask;
-  std::vector<uint32_t> false_positives(num_input_bits, 0);
-  std::vector<uint32_t> num_leaves(num_input_bits, 0);
-  for (size_t i = 1; i < n; i++) {
-    std::cout << "b "; print(b, i-1);
-    std::bitset<num_input_bits> t = b >> i;
-    std::cout << "t "; print(t, 0);
-    std::bitset<num_input_bits> d = b ^ t;
-    std::cout << "d "; print(d, i);
-    b |= t;
-    std::cout << "b "; print(b, i);
-    for (size_t j = 0; j < num_input_bits; j += 1 << i) {
-      false_positives[j] = d[j] * (1 << (i-1))  + (false_positives[j] + false_positives[j + (1 << (i-1))]);
-      num_leaves[j] = !d[j] * (num_leaves[j] + num_leaves[j + (1 << (i-1))]) + 2* d[j];
+        u1 prune_causes_false_positives = left_bit ^ right_bit;
+        u1 both_nodes_are_leaves = !is_inner_node[left_node_idx] & !is_inner_node[right_node_idx];
+        u1 prune = both_nodes_are_leaves & !prune_causes_false_positives;
+        if (prune) {
+          is_inner_node[parent_node_idx] = false;
+        }
+        else {
+          if (prune_causes_false_positives) {
+            u64 left_fp = !left_bit * (1 << (height - level_of(left_node_idx)));
+            u64 right_fp = !right_bit * (1 << (height - level_of(right_node_idx)));
+            false_positive_cnt[parent_node_idx] = false_positive_cnt[left_node_idx] + false_positive_cnt[right_node_idx]
+                                                  + left_fp + right_fp;
+          }
+        }
+      }
+      std::cout << "done w/ bottom up pruning" << std::endl;
+      assert(bitmask.count() == 0 || false_positive_cnt[0] == N - bitmask.count());
     }
-    std::cout << "false positives "; print(false_positives, i);
-    std::cout << "num leaves      "; print(num_leaves, 0);
-  }
-  std::cout << false_positives[0] << std::endl;
-  std::cout << compressed_bitmask[0] << std::endl;
 
-  return compressed_bitmask;
+    std::vector<$u1> encode() {
+      std::vector<$u1> structure;
+      std::vector<$u1> labels;
+      std::function<void(u64)> encode_recursively = [&](u64 idx) {
+        u1 is_inner = is_inner_node[idx];
+        structure.push_back(true);
+        if (is_inner) {
+          encode_recursively(left_child_of(idx));
+          encode_recursively(right_child_of(idx));
+        }
+        else {
+          labels.push_back(bit[idx]);
+        }
+        structure.push_back(false);
+      };
+      encode_recursively(0);
+      // append the labels
+      std::copy(labels.begin(), labels.end(), std::back_inserter(structure));
+      return structure;
+    }
+
+    void compress(u64 target_bit_cnt) {
+      assert(target_bit_cnt > 2);
+
+      std::function<u64(u64)> node_cnt = [&](u64 node_idx) -> u64 {
+        if (!is_inner_node[node_idx]) return 1;
+        return node_cnt(left_child_of(node_idx)) + node_cnt(right_child_of(node_idx));
+      };
+
+      std::function<u64(u64, u64)> prune_single = [&](u64 node_idx, u64 safe_bits) {
+        assert(is_inner_node[node_idx]);
+
+        u64 left_child_idx = left_child_of(node_idx);
+        u64 right_child_idx = right_child_of(node_idx);
+
+        u64 left_sub_tree_size = node_cnt(left_child_idx);
+        u64 right_sub_tree_size = node_cnt(right_child_idx);
+
+        if (left_sub_tree_size == 1 && right_sub_tree_size == 1) {
+          is_inner_node[node_idx] = false;
+          return node_idx;
+        }
+
+        if (left_sub_tree_size == 1) {
+          return prune_single(right_child_idx, safe_bits);
+        }
+
+        if (right_sub_tree_size == 1) {
+          return prune_single(left_child_idx, safe_bits);
+        }
+
+        u64 left_false_positive_cnt = false_positive_cnt[left_child_idx];
+        u64 right_false_positive_cnt = false_positive_cnt[right_child_idx];
+
+        f64 left_trade = left_sub_tree_size / (left_false_positive_cnt + 0.01);
+        f64 right_trade = right_sub_tree_size / (right_false_positive_cnt + 0.01);
+
+        return prune_single(left_child_idx + (right_trade > left_trade), safe_bits);
+      };
+
+      std::function<void(u64)> prune_clean = [&](u64 node_idx) {
+        assert(!is_inner_node[node_idx]);
+        $u64 current_node_idx = node_idx;
+        while (current_node_idx != 0) {
+          current_node_idx = parent_of(current_node_idx);
+          u64 left_child_idx = left_child_of(node_idx);
+          u64 right_child_idx = right_child_of(node_idx);
+          if (bit[left_child_idx] == bit[right_child_idx]) {
+            is_inner_node[current_node_idx] = false;
+            std::cout << "cleanup" << std::endl;
+          }
+          else {
+            break;
+          }
+        }
+      };
+
+      while (true) {
+        auto enc = encode();
+        u64 current_bit_cnt = enc.size();
+        std::cout << current_bit_cnt << ": "; print(enc, 0);
+        if (current_bit_cnt <= target_bit_cnt) break;
+        prune_clean(prune_single(0, current_bit_cnt - target_bit_cnt));
+      }
+      std::cout << "---" << std::endl;
+    }
+
+    static u64 find_close(const std::vector<$u1> bitstring, u64 idx) {
+      if (!bitstring[idx]) return idx;
+      $u64 cntr = 1;
+      for ($u64 i = idx + 1; i < bitstring.size(); i++) {
+        bitstring[i] ? cntr++ : cntr--;
+        if (cntr == 0) return i;
+      }
+      return idx;
+    }
+
+    template<u64 M>
+    static u64 find_close(const std::bitset<M> bitstring, u64 idx) {
+      if (!bitstring[idx]) return idx;
+      $u64 cntr = 1;
+      for ($u64 i = idx + 1; i < M; i++) {
+        bitstring[i] ? cntr++ : cntr--;
+        if (cntr == 0) return i;
+      }
+      return idx;
+    }
+
+    static inline void write(std::bitset<N>& bitmask, u64 offset, u1 bit, u64 cnt) {
+      for ($u64 i = 0; i < cnt; i++) {
+        bitmask[offset + i] = bit;
+      }
+    }
+
+
+  public:
+
+    /// Encodes the given bitmask as a full binary tree using balanced parentheses representation.
+    /// @returns a bit vector of variable size containing the encoded 'tree mask'
+    static std::vector<$u1> encode(const std::bitset<N> bitmask) {
+      auto t = tree_mask(bitmask);
+      return t.encode();
+    }
+
+    /// Decodes the given 'tree mask'.
+    /// @returns a bitmask of fixed size
+    static std::bitset<N> decode(const std::vector<$u1> code) {
+      std::bitset<N> bitmask;
+      u64 labels_offset = find_close(code, 0) + 1;
+      u64 height = ct::log_2<N>::value;
+      $u64 write_pos = 0;
+      $u64 level = 0;
+      $u1 previous_bit = code[0];
+      for ($u64 i = 1, j = labels_offset; i < labels_offset; i++) {
+        u1 current_bit = code[i];
+        u1 is_leaf = ! current_bit & previous_bit;
+        if (is_leaf) {
+          u64 n = 1 << (height - level);
+          u1 label = code[j];
+          write(bitmask, write_pos, label, n);
+          write_pos += n;
+          j++;
+        }
+        current_bit ? level++ : level--;
+        previous_bit = current_bit;
+      }
+      return bitmask;
+    }
+
+    /// Encodes and compresses the given bitmask as a full binary tree using balanced parentheses representation.
+    /// The length of encoded tree mask is guaranteed to be less or equal to M.
+    /// Note, that the compression can lead to an information loss. However, the following holds: m == m & d(e(m)
+    /// @returns a bit set of size M containing the encoded 'tree mask'
+    template<u64 M>
+    static std::bitset<M> compress(const std::bitset<N> bitmask) {
+      auto tree = tree_mask(bitmask);
+      tree.compress(M);
+      auto compressed_bitvector = tree.encode();
+      std::bitset<M> compressed_bitmask;
+      for ($u64 i = 0; i < compressed_bitvector.size(); i++) {
+        compressed_bitmask[i] = compressed_bitvector[i];
+      }
+      return compressed_bitmask;
+    }
+
+
+    /// Decodes the given 'tree mask' of length M into a bitmask of length N.
+    /// @returns a bitmask of fixed size
+    template<u64 M>
+    static std::bitset<N> decode(const std::bitset<M> code) {
+      std::bitset<N> bitmask;
+      u64 labels_offset = find_close(code, 0) + 1;
+      u64 height = ct::log_2<N>::value;
+      $u64 write_pos = 0;
+      $u64 level = 0;
+      $u1 previous_bit = code[0];
+      for ($u64 i = 1, j = labels_offset; i < labels_offset; i++) {
+        u1 current_bit = code[i];
+        u1 is_leaf = ! current_bit & previous_bit;
+        if (is_leaf) {
+          u64 n = 1 << (height - level);
+          u1 label = code[j];
+          write(bitmask, write_pos, label, n);
+          write_pos += n;
+          j++;
+        }
+        current_bit ? level++ : level--;
+        previous_bit = current_bit;
+      }
+      return bitmask;
+    }
+
+  };
 }
--- test/bitmask_test.cpp
+++ test/bitmask_test.cpp
@@ -1,14 +1,102 @@
 #include "gtest/gtest.h"
-
-#include <bitset>
+#include "../adept.hpp"
 #include "../bitmask.hpp"
+#include <bitset>
+#include <functional>
+#include <iostream>
+#include <random>
 
+using namespace tree;
 
-TEST(bitmask, tree_compress) {
-  std::bitset<8> bitmask;//("01100011");
-  bitmask.set(1);
-  bitmask.set(2);
-  bitmask.set(6);
-  bitmask.set(7);
-  std::bitset<64> compressed_bitmask = compress<8, 64>(bitmask);
+static std::vector<$u1> bitvector(const std::string bit_string) {
+  std::vector<$u1> bv;
+  for ($u64 i = 0; i < bit_string.size(); i++) {
+    bv.push_back(bit_string[i] != '0');
+  }
+  return bv;
+}
+
+TEST(bitmask, tree_encode) {
+  ASSERT_EQ(bitvector("11010010"), tree_mask<16>::encode(std::bitset<16>("0000000011111111")));
+  ASSERT_EQ(bitvector("11010010"), tree_mask<8>::encode(std::bitset<8>("00001111")));
+  ASSERT_EQ(bitvector("11010010"), tree_mask<2>::encode(std::bitset<2>("01")));
+  ASSERT_EQ(bitvector("100"), tree_mask<4>::encode(std::bitset<4>("0000")));
+  ASSERT_EQ(bitvector("101"), tree_mask<4>::encode(std::bitset<4>("1111")));
+  ASSERT_EQ(bitvector("1110100100101"), tree_mask<8>::encode(std::bitset<8>("11110011")));
+  ASSERT_EQ(bitvector("1110100100101"), tree_mask<4>::encode(std::bitset<4>("1101")));
+  ASSERT_EQ(bitvector("111010011010001010"), tree_mask<4>::encode(std::bitset<4>("0101")));
+  ASSERT_EQ(bitvector("111010011010001010"), tree_mask<32>::encode(std::bitset<32>("00000000111111110000000011111111")));
+  //ASSERT_EQ(bitvector(""), tree::tree_mask<>::encode(std::bitset<>("")));
+}
+
+TEST(bitmask, tree_decode) {
+  ASSERT_EQ(std::bitset<16>("0000000011111111"), tree_mask<16>::decode(bitvector("11010010")));
+  ASSERT_EQ(std::bitset<8>("00001111"), tree_mask<8>::decode(bitvector("11010010")));
+  ASSERT_EQ(std::bitset<2>("01"), tree_mask<2>::decode(bitvector("11010010")));
+  ASSERT_EQ(std::bitset<4>("0000"), tree_mask<4>::decode(bitvector("100")));
+  ASSERT_EQ(std::bitset<4>("1111"), tree_mask<4>::decode(bitvector("101")));
+  ASSERT_EQ(std::bitset<8>("11110011"), tree_mask<8>::decode(bitvector("1110100100101")));
+  ASSERT_EQ(std::bitset<4>("1101"), tree_mask<4>::decode(bitvector("1110100100101")));
+  ASSERT_EQ(std::bitset<4>("0101"), tree_mask<4>::decode(bitvector("111010011010001010")));
+  ASSERT_EQ(std::bitset<32>("00000000111111110000000011111111"), tree_mask<32>::decode(bitvector("111010011010001010")));
+}
+
+template<u64 N>
+static void test_enc_dec(std::bitset<N> bitmask) {
+  ASSERT_EQ(bitmask, tree_mask<N>::decode(tree_mask<N>::encode(bitmask)));
+};
+
+TEST(bitmask, tree_encode_decode) {
+  test_enc_dec(std::bitset<16>("1111111111111111"));
+  test_enc_dec(std::bitset<16>("0000000011111111"));
+  test_enc_dec(std::bitset<16>("1111111100000000"));
+  test_enc_dec(std::bitset<16>("1111000011110000"));
+  test_enc_dec(std::bitset<16>("0000111100001111"));
+  test_enc_dec(std::bitset<16>("0011001100110011"));
+  test_enc_dec(std::bitset<16>("1100110011001100"));
+  test_enc_dec(std::bitset<16>("1010101010101010"));
+  test_enc_dec(std::bitset<16>("0101010101010101"));
+  test_enc_dec(std::bitset<16>("0000000000000000"));
+  test_enc_dec(std::bitset<4>("1111"));
+  test_enc_dec(std::bitset<4>("1100"));
+  test_enc_dec(std::bitset<4>("0011"));
+  test_enc_dec(std::bitset<4>("0101"));
+  test_enc_dec(std::bitset<4>("1010"));
+  test_enc_dec(std::bitset<4>("0000"));
+  test_enc_dec(std::bitset<1>("1"));
+  test_enc_dec(std::bitset<1>("0"));
+}
+
+template<u64 LEN>
+static void assert_no_false_negatives(std::bitset<LEN> exact_bitmask, std::bitset<LEN> other_bitmask) {
+  ASSERT_EQ(exact_bitmask, exact_bitmask & other_bitmask);
+}
+
+/// Typical blocks contain 2^17 values of type 1, 2 or 4 byte integers.
+/// As we need at most cache-line granularity, bitmasks are of size 2^11, 2^12 or 2^13.
+constexpr u64 N = 1 << 11; // 12 13;
+
+template<u64 M>
+static void test_compression(std::bitset<N> bitmask) {
+  assert_no_false_negatives(bitmask, tree_mask<N>::decode<M>(tree_mask<N>::compress<M>(bitmask)));
+};
+
+TEST(bitmask, compression) {
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<u64> dis(1, 1000);
+
+  std::bitset<N> bitmask;
+  for ($u64 i = 0; i < N; i++) {
+    if (dis(gen) <= 1) {
+      bitmask.set(i);
+    }
+  }
+  print(bitmask, 0);
+  test_compression<1024>(bitmask);
+  test_compression<512>(bitmask);
+  test_compression<256>(bitmask);
+  test_compression<128>(bitmask);
+  test_compression<64>(bitmask);
 }
