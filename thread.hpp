d6c109df39a20f1b7e2f31b479ed5bc65c0c9e1d
Harald Lang <harald.lang@in.tum.de>
2017-03-10 16:37:42 +0100

    fix reference capturing in 'run_in_parallel'

--- thread.hpp
+++ thread.hpp
@@ -22,19 +22,43 @@ void
 run_in_parallel(std::function<void()> fn,
                 u32 thread_cnt = std::thread::hardware_concurrency()) {
 
-  auto thread_fn = [&](u32 thread_id) {
+  auto thread_fn = [](u32 thread_id, std::function<void()> fn) {
     thread_affinitize(thread_id);
     fn();
   };
 
   std::vector<std::thread> workers;
   for (std::size_t i = 0; i < thread_cnt - 1; i++) {
-    workers.push_back(std::move(std::thread(thread_fn, i)));
+    workers.push_back(std::move(std::thread(thread_fn, i, fn)));
   }
-  std::thread(thread_fn, thread_cnt - 1).join();
+  std::thread(thread_fn, thread_cnt - 1, fn).join();
   for (auto& worker : workers) {
     worker.join();
   }
+}
+
+void
+run_in_parallel_async(std::function<void()> fn,
+                      std::vector<std::thread>& workers,
+                      u32 thread_cnt = std::thread::hardware_concurrency()) {
+  workers.reserve(thread_cnt);
+
+  auto thread_fn = [](u32 thread_id, std::function<void()> fn) {
+    thread_affinitize(thread_id);
+    fn();
+  };
+
+  for (std::size_t i = 0; i < thread_cnt; i++) {
+    workers.push_back(std::move(std::thread(thread_fn, i, fn)));
+  }
+}
+
+void
+wait_for_threads(std::vector<std::thread>& workers) {
+  for (auto& worker : workers) {
+    worker.join();
+  }
+  workers.clear();
 }
 
 } // namespace dtl
