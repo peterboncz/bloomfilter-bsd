0dce4d32214910998752d6143a0fce96181e4b9f
Harald Lang <harald.lang@in.tum.de>
2017-04-05 15:23:49 +0200

    add crc32 to hash functions

--- hash.hpp
+++ hash.hpp
@@ -1,13 +1,21 @@
 #pragma once
 
 #include "adept.hpp"
+#include "nmmintrin.h"
 
 namespace dtl {
 namespace hash {
 
+template<typename T, u32 seed = 1337>
+struct crc32 {
+  static inline constexpr T hash(const T& key) {
+    return _mm_crc32_u32(key, seed);
+  }
+};
+
 template<typename T>
 struct xorshift_64 {
-  static constexpr T hash(const T& key) {
+  static inline constexpr T hash(const T& key) {
     T h = key;
     h ^= h << 13;
     h ^= h >> 7;
@@ -17,10 +25,10 @@ struct xorshift_64 {
 };
 
 
-template<typename T>
+template<typename T, u32 seed = 0xc6a4a793u>
 struct murmur1_32 {
-  static constexpr T hash(const T& key) {
-    const T m = 0xc6a4a793u;
+  static inline constexpr T hash(const T& key) {
+    const T m = seed;
     const T hi = 0x4e774912u ^(4 * m);
     T h = hi;
     h += key;
@@ -35,10 +43,10 @@ struct murmur1_32 {
 };
 
 
-template<typename T>
+template<typename T, u64 seed = 0xc6a4a7935bd1e995ull>
 struct murmur64a_64 {
-  static constexpr T hash(const T& key) {
-    const T m = 0xc6a4a7935bd1e995ull;
+  static inline constexpr T hash(const T& key) {
+    const T m = seed;
     const T r = 47u;
     const T hi = 0x8445d61a4e774912ull ^ (8 * m);
     T h = hi;
