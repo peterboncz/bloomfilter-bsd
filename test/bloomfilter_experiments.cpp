91d2e66cefa13e6c615dbfb3c5928c4aa99535f5
Harald Lang <harald.lang@in.tum.de>
2017-04-06 16:30:41 +0200

    cleanup bloomfilter experiments

--- test/bloomfilter_experiments.cpp
+++ test/bloomfilter_experiments.cpp
@@ -5,8 +5,6 @@
 #include "../simd.hpp"
 #include <chrono>
 
-//#include "benchmark/benchmark_api.h"
-
 using namespace dtl;
 
 struct xorshift32 {
@@ -75,7 +73,7 @@ using murmur = dtl::hash::murmur1_32<T>;
 using bf_t = dtl::bloomfilter<$u32, dtl::hash::knuth, murmur>;
 
 TEST(bloom, filter_performance) {
-  u64 repeat_cnt = 1u << 25;
+  u64 repeat_cnt = 1u << 28;
   bf_t bf((1ull << 24) -1);
   {
 //    xorshift32 prng;
@@ -106,80 +104,3 @@ TEST(bloom, filter_performance) {
   }
 
 }
-
-/*
-static void benchmark_bloomfilter_probe(benchmark::State& state) {
-  u64 element_cnt = 1 << (state.range(0) / 8);
-  std::unique_ptr<bf_t> bf;
-
-  // setup
-  if (state.thread_index == 0) {
-    bf = std::make_unique<bf_t>(1ull << state.range(0));
-    xorshift32 prng;
-    for ($u64 i = 0; i < element_cnt; i++) {
-      prng();
-      bf->insert(prng.x32);
-    }
-  }
-
-  $u64 found = 0;
-  xorshift32 prng(state.thread_index);
-  while (state.KeepRunning()) {
-    prng();
-    found += bf->contains(prng.x32);
-  }
-  state.SetItemsProcessed(state.iterations());
-
-  // teardown
-  if (state.thread_index == 0) {
-  }
-
-}
-
-static void benchmark_bloomfilter_probe_vectorized(benchmark::State& state) {
-  u64 vector_len = 8;
-  using bf_t = dtl::bloomfilter<$i32, dtl::hash::xorshift_64>;
-  u64 element_cnt = 1 << (state.range(0) / 8);
-  std::unique_ptr<bf_t> bf;
-
-  // setup
-  if (state.thread_index == 0) {
-    bf = std::make_unique<bf_t>(1ull << state.range(0));
-    xorshift32 prng;
-    for ($u64 i = 0; i < element_cnt; i++) {
-      prng();
-      bf->insert(prng.x32);
-    }
-  }
-
-
-//  vec<$u32, vector_len> found;
-//  vec<$u32, vector_len> rnd_keys;
-  vec<$i32, vector_len> found;
-  vec<$i32, vector_len> rnd_keys;
-  xorshift32 prng(state.thread_index);
-  for ($u64 i = 0; i < vector_len; i++) {
-    found.insert(i, 0);
-    rnd_keys.insert(i, prng.x32);
-    prng();
-  }
-
-  while (state.KeepRunning()) {
-    xorshift32::next(rnd_keys);
-
-    //found +=
-        bf->contains(rnd_keys);
-  }
-  state.SetItemsProcessed(state.iterations() * vector_len);
-
-  // teardown
-  if (state.thread_index == 0) {
-
-  }
-
-}
-
-BENCHMARK(benchmark_bloomfilter_probe)->DenseRange(8, 32)->ThreadPerCpu()->UseRealTime();
-BENCHMARK(benchmark_bloomfilter_probe_vectorized)->DenseRange(8, 32)->ThreadPerCpu()->UseRealTime();
-BENCHMARK_MAIN();
- */
