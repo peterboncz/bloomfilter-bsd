02417a67c7c89297359d6ffa4f82f889a9a1dad6
Harald Lang <harald.lang@in.tum.de>
2017-04-07 01:03:14 +0200

    add bloomfilter benchmarks (scalar vs autovec)

--- test/bloomfilter_experiments.cpp
+++ test/bloomfilter_experiments.cpp
@@ -11,7 +11,9 @@ struct xorshift32 {
   $u32 x32;
   xorshift32() : x32(314159265) { };
   xorshift32(u32 seed) : x32(seed) { };
-  inline u32 operator()() {
+
+  inline u32
+  operator()() {
     x32 ^= x32 << 13;
     x32 ^= x32 >> 17;
     x32 ^= x32 << 5;
@@ -19,7 +21,8 @@ struct xorshift32 {
   }
 
   template<typename T>
-  static inline void next(T& x32) {
+  static inline void
+  next(T& x32) {
     x32 ^= x32 << 13;
     x32 ^= x32 >> 17;
     x32 ^= x32 << 5;
@@ -47,23 +50,74 @@ TEST(bloom, prng_performance) {
 }
 
 
-TEST(bloom, hash_performance) {
-  u64 repeat_cnt = 1u << 28;
+template<typename hash_fn_t>
+void run_hash_benchmark(hash_fn_t hash_fn,
+                        const std::size_t input_size = 1ull << 10 /* prevent auto-vectorization*/ ) {
+  // prepare input
+  std::vector<$u32> input;
   xorshift32 prng;
-//  using hash_fn = dtl::hash::xorshift_64<u32>;
-//  using hash_fn = dtl::hash::murmur1_32<u32>;
-//  using hash_fn = dtl::hash::crc32<u32>;
-  using hash_fn = dtl::hash::knuth<u32>;
-  $u32 hash = 0;
+  for (std::size_t i = 0; i < input_size; i++) {
+    input.push_back(prng());
+  }
+
+  // run benchmark
+  u64 repeat_cnt = 100000;
+  $u64 chksum = 0;
   double duration = timing([&] {
-    for ($u64 i = 0; i != repeat_cnt; i++) {
-//      prng();
-//      hash ^= hash_fn::hash(prng.x32);
-      hash ^= hash_fn::hash(i);
+    for ($u64 r = 0; r != repeat_cnt; r++) {
+      for ($u64 i = 0; i != input_size; i++) {
+        chksum += hash_fn.hash(input[i]);
+      }
     }
   });
-  double perf = (repeat_cnt) / (duration / 1000.0);
-  std::cout << perf << " [hashes/sec]    (" << prng.x32 << ")" << std::endl;
+  u64 perf = (input_size * repeat_cnt) / (duration / 1000.0);
+  std::cout << perf << " [hashes/sec]    (chksum: " << chksum << ")" << std::endl;
+}
+
+
+template<typename hash_fn_t>
+void run_hash_benchmark_autovec(hash_fn_t hash_fn) {
+  // prepare input
+  const std::size_t input_size = 1ull << 10;
+  std::vector<$u32> input;
+  xorshift32 prng;
+  for (std::size_t i = 0; i < input_size; i++) {
+    input.push_back(prng());
+  }
+
+  // run benchmark
+  u64 repeat_cnt = 100000;
+  $u64 chksum = 0;
+  u64 duration = timing([&] {
+    for ($u64 r = 0; r != repeat_cnt; r++) {
+      for ($u64 i = 0; i != input_size; i++) {
+        chksum += hash_fn.hash(input[i]);
+      }
+    }
+  });
+  u64 perf = (input_size * repeat_cnt) / (duration / 1000.0);
+  std::cout << perf << " [hashes/sec]    (chksum: " << chksum << ")" << std::endl;
+}
+
+
+TEST(bloom, hash_performance) {
+  std::cout.setf(std::ios::fixed);
+  std::cout << "xorshift   " << std::endl;
+  dtl::hash::xorshift_64<u32> xorshift_64;
+  run_hash_benchmark(xorshift_64);
+  run_hash_benchmark_autovec(xorshift_64);
+  std::cout << "murmur1_32 " << std::endl;
+  dtl::hash::murmur1_32<u32> murmur1_32;
+  run_hash_benchmark(murmur1_32);
+  run_hash_benchmark_autovec(murmur1_32);
+  std::cout << "crc32      " << std::endl;
+  dtl::hash::crc32<u32> crc32;
+  run_hash_benchmark(crc32);
+  run_hash_benchmark_autovec(crc32);
+  std::cout << "knuth      " << std::endl;
+  dtl::hash::knuth<u32> knuth;
+  run_hash_benchmark(knuth);
+  run_hash_benchmark_autovec(knuth);
 }
 
 
