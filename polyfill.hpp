f87e3f0c636a9f231fd4e67739ba5c84664d564d
Harald Lang <harald.lang@in.tum.de>
2017-05-01 11:56:45 +0200

    fix missing include

--- polyfill.hpp
+++ polyfill.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
+#include <type_traits>
 
 #if __cplusplus < 201402L
 namespace std {
 
-template< bool B, class T = void >
+template<bool B, class T = void>
 using enable_if_t = typename enable_if<B,T>::type;
 
 }
