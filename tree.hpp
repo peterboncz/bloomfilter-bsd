88239380570cb6f77261dd8edbd65a708e4cb0d3
Harald Lang <harald.lang@in.tum.de>
2018-05-17 12:01:57 +0200

    cleanup

--- tree.hpp
+++ tree.hpp
@@ -15,7 +15,7 @@ namespace dtl {
     static constexpr u64 max_node_cnt = 2 * N - 1;
     static constexpr u64 height = ct::log_2<N>::value;
 
-  private:
+//  private:
 
     std::bitset<max_node_cnt> _is_inner_node;
 
@@ -69,39 +69,7 @@ namespace dtl {
       _is_inner_node[node_idx] = true;
     }
 
-    void
-    dot() const {
-      std::function<void(u64)> dot_recursively = [&](u64 idx) {
-        using tree_t = decltype(*this);
-        u1 is_inner = is_inner_node(idx);
-        std::cout << "n" << idx << "[label=\"foo" << "\",style=filled,color=\"" << (true ? "#abd600" : "#b50000") << "\",width=0.2]" <<std::endl;
-//          std::cout << "n" << idx << std::endl;
-
-        u64 left_child_idx = left_child_of(idx);
-        if (left_child_idx < max_node_cnt) {
-          std::cout << "n" << idx << " -- n" << left_child_idx << std::endl; // << "[weight=0]"
-          dot_recursively(left_child_idx);
-        }
-
-        u64 right_child_idx = right_child_of(idx);
-        if (right_child_idx < max_node_cnt) {
-          std::cout << "n" << idx << " -- n" << right_child_idx << std::endl;
-          dot_recursively(right_child_idx);
-        }
-      };
-      std::cout << "graph \"\" {" << std::endl;
-      dot_recursively(0);
-      std::cout << "}" << std::endl;
-    }
-
-    inline void
-    print(std::ostream& os) const {
-      os << _is_inner_node;
-    }
-
   };
-
-
 
   template<u64 N>
   using full_binary_tree = binary_tree_structure<N>;
