d6e965fc953f0ddcc0b0f4cd314ec768a7ffb7a4
Harald Lang <harald.lang@in.tum.de>
2018-07-05 13:55:12 +0200

    add print function

--- bloomfilter/blocked_bloomfilter_config.hpp
+++ bloomfilter/blocked_bloomfilter_config.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <sstream>
+
 #include <dtl/dtl.hpp>
 #include <dtl/bloomfilter/block_addressing_logic.hpp>
 
@@ -22,6 +24,18 @@ struct blocked_bloomfilter_config {
         || (k == o.k && word_size == o.word_size && word_cnt_per_block == o.word_cnt_per_block && sector_cnt < o.sector_cnt)
         || (k == o.k && word_size == o.word_size && word_cnt_per_block == o.word_cnt_per_block && sector_cnt == o.sector_cnt && addr_mode < o.addr_mode)
         || (k == o.k && word_size == o.word_size && word_cnt_per_block == o.word_cnt_per_block && sector_cnt == o.sector_cnt && addr_mode == o.addr_mode && zone_cnt < o.zone_cnt);
+  }
+
+  void print(std::ostream& os) const {
+    std::stringstream str;
+    str << "bbf"
+        << ",k=" << k
+        << ",word_size=" << word_size
+        << ",word_cnt=" << word_cnt_per_block
+        << ",sector_cnt=" << sector_cnt
+        << ",addr=" << (addr_mode == dtl::block_addressing::POWER_OF_TWO ? "pow2" : "magic")
+        << ",zone_cnt=" << zone_cnt;
+    os << str.str();
   }
 
 };
