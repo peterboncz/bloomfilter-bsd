cbf9f4609bc92056068e67eeba2092593af5974d
Harald Lang <harald.lang@in.tum.de>
2018-07-07 00:21:48 +0200

    remove redundant cuckoo config definition

--- filter/cuckoofilter/cuckoofilter_tune.hpp
+++ filter/cuckoofilter/cuckoofilter_tune.hpp
@@ -8,25 +8,10 @@
 #include <dtl/filter/blocked_bloomfilter/block_addressing_logic.hpp>
 
 #include "immintrin.h"
+#include "cuckoofilter_config.hpp"
 
 namespace dtl {
 namespace cuckoofilter {
-
-namespace {
-
-struct config {
-  u32 bits_per_tag;
-  u32 tags_per_bucket;
-  dtl::block_addressing addr_mode;
-
-  bool
-  operator<(const config &o) const {
-    return bits_per_tag < o.bits_per_tag
-        || (bits_per_tag == o.bits_per_tag && tags_per_bucket  < o.tags_per_bucket)
-        || (bits_per_tag == o.bits_per_tag && tags_per_bucket == o.tags_per_bucket && addr_mode  < o.addr_mode);
-  }
-
-};
 
 struct tuning_params {
   u32 unroll_factor;
