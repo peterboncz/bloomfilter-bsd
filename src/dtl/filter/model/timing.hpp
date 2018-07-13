8ab2f414613a9056f39a3f9302fe11f0f2447429
Harald Lang <harald.lang@in.tum.de>
2018-07-13 18:12:56 +0200

    add arithmetic operators

--- src/dtl/filter/model/timing.hpp
+++ src/dtl/filter/model/timing.hpp
@@ -13,10 +13,71 @@ struct timing {
   $f64 cycles_per_lookup = 0.0;
   $f64 nanos_per_lookup = 0.0;
 
-  $u1 operator==(const timing& other) const {
+  $u1
+  operator==(const timing& other) const {
     return cycles_per_lookup == other.cycles_per_lookup
         && nanos_per_lookup == other.nanos_per_lookup;
   }
+
+  $u1
+  operator<(const timing& other) const {
+    return cycles_per_lookup < other.cycles_per_lookup;
+  }
+
+  timing
+  operator*(f64 rhs) const {
+    timing t;
+    t.cycles_per_lookup = cycles_per_lookup * rhs;
+    t.nanos_per_lookup = nanos_per_lookup * rhs;
+    return t;
+  }
+
+  timing
+  operator/(f64 rhs) const {
+    timing t;
+    t.cycles_per_lookup = cycles_per_lookup / rhs;
+    t.nanos_per_lookup = nanos_per_lookup / rhs;
+    return t;
+  }
+
+  timing
+  operator+(f64 rhs) const {
+    timing t;
+    t.cycles_per_lookup = cycles_per_lookup + rhs;
+    t.nanos_per_lookup = nanos_per_lookup + rhs;
+    return t;
+  }
+
+  timing&
+  operator+=(f64 rhs) {
+    cycles_per_lookup += rhs;
+    nanos_per_lookup += rhs;
+    return *this;
+  }
+
+  timing
+  operator+(const timing& rhs) const {
+    timing t;
+    t.cycles_per_lookup = cycles_per_lookup + rhs.cycles_per_lookup;
+    t.nanos_per_lookup = nanos_per_lookup + rhs.nanos_per_lookup;
+    return t;
+  }
+
+  timing&
+  operator+=(const timing& rhs) {
+    cycles_per_lookup += rhs.cycles_per_lookup;
+    nanos_per_lookup += rhs.nanos_per_lookup;
+    return *this;
+  }
+
+  timing
+  operator-(const timing& rhs) const {
+    timing t;
+    t.cycles_per_lookup = cycles_per_lookup - rhs.cycles_per_lookup;
+    t.nanos_per_lookup = nanos_per_lookup - rhs.nanos_per_lookup;
+    return t;
+  }
+
 };
 //===----------------------------------------------------------------------===//
 
