fb67fade6a57acfea3dd6d02169bd3add6de2e68
Harald Lang <harald.lang@in.tum.de>
2019-04-07 18:16:32 +0200

    add more CUDA API helper functions

--- amsfilter/src/amsfilter/cuda/internal/cuda_api_helper.hpp
+++ amsfilter/src/amsfilter/cuda/internal/cuda_api_helper.hpp
@@ -2,6 +2,7 @@
 
 #include <cuda_runtime.h>
 
+//===----------------------------------------------------------------------===//
 // taken from: https://codeyarns.com/2011/03/02/how-to-do-error-checking-in-cuda/
 #define cuda_check_error()  __cuda_check_error( __FILE__, __LINE__ )
 __forceinline__
@@ -14,4 +15,18 @@ __cuda_check_error(const char *file, const int line ) {
     exit(-1);
   }
 }
-
+//===----------------------------------------------------------------------===//
+static std::string
+get_cuda_device_name(u32 cuda_device_no) {
+  cudaDeviceProp device_prop;
+  cudaGetDeviceProperties(&device_prop, cuda_device_no);
+  return std::string(device_prop.name);
+}
+//===----------------------------------------------------------------------===//
+static u32
+get_cuda_device_count() {
+  $i32 count;
+  cudaError_t cudaGetDeviceCount(&count);
+  return static_cast<u32>(count);
+}
+//===----------------------------------------------------------------------===//
